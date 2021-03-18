7b7ce7b702f4ea94b1467f0b2efffc0acdf562e7
unknown <pawanmourya568@gmail.com>
2021-03-18 18:51:01 +0530

    added new route menu

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.get('/', (req,res)=> {
     res.send("welcome to my youtube chanel")
 })
 
+app.get('/menu', (req,res)=> {
+    res.send("this is menu page")
+})
 app.listen(3000, ()=> {
     console.log("listning")
 })
